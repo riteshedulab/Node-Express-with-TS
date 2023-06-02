cf853b2551b0fee5030936eb29cb69a25d05d589
riteshyadav@edulab.in <riteshyadav@edulab.in>
2023-06-02 16:11:54 +0530

    Code Push on New branch

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ const app = express();
 const PORT = 4000;
 
 
-// app.get('/',(req,res)=>{
-//     console.log()
-// })
+app.get('/',(req,res)=>{
+    console.log()
+})
+
+app.get('/Home',(req,res)=>{
+    console.log("Home Page")
+})
+
 app.listen(PORT,()=>{
     console.log('sever or listening on ${PORT}')
 })
